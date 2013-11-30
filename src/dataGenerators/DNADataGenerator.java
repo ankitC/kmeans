3199aa1c0d8397afde457fade3660b2a3e7437a4
Ankit <ankit.ac.chheda@gmail.com>
2013-11-30 03:00:46 -0500

    Adding DNA data generator

--- src/dataGenerators/DNADataGenerator.java
+++ src/dataGenerators/DNADataGenerator.java
@@ -1,21 +1,32 @@
 package dataGenerators;
 
+import interfaces4KMeans.DataPoint;
+
+import java.util.ArrayList;
 import java.util.Random;
+
+import DNASampleSpace.DataPointDNA;
 
 
 public class DNADataGenerator {
-	
-    private static String element[] = {"A", "C", "G", "T"};
-    private static Random random  = new Random();
 
-    public static String[] generateDNA(int length){
-        String[] dnaStrand = new String[length];
+	private static String element[] = {"A", "C", "G", "T"};
+	private static Random random  = new Random();
 
-        for(int i=0; i<length; i++){
-            String prop = element[random.nextInt()%4];
-            dnaStrand[i] = prop;
-        }
-        return dnaStrand;
-    }
+	public static ArrayList<DataPoint> generateDNA(int numberOfStrands, int elementsPerStrand){
 
+		String[] dnaStrand = new String[elementsPerStrand];
+		ArrayList<DataPoint> returnCollection = new ArrayList<DataPoint>();
+		
+		for(int i=0; i< numberOfStrands; i++){
+			for(int j=0; j<elementsPerStrand; j++){
+				String prop = element[Math.abs(random.nextInt())%4];
+				dnaStrand[j] = prop;
+			}
+			returnCollection.add(new DataPointDNA(dnaStrand));
+		}
+		
+
+		return returnCollection;
+	}
 }
