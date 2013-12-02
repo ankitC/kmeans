4abb6b57765fbf439fdd1dacd161d8576ddc350f
Ankit <ankit.ac.chheda@gmail.com>
2013-12-02 03:00:23 -0500

    Adding script to compile the code on GHC machines

---
+++ src/compileMPI.sh
@@ -0,0 +1,1 @@
+javac src/KMeansParallel.java src/dataGenerators/DNADataGenerator.java src/DNASampleSpace/AverageDNA.java src/DNASampleSpace/DataPointDNA.java src/interfaces4KMeans/*.java src/KMeans/KMeansCluster.java src/KMeans/Parallel/*.java -d ./bin/
